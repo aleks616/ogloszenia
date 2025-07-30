264f99c509fa9ed2266bd38e24f706ed258f89f7
aleks616 <154893459+aleks616@users.noreply.github.com>
2025-07-30 23:09:52 +0200

    dodano dump bazy

---
+++ ogloszeniadb.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: ogloszeniadb
+-- ------------------------------------------------------
+-- Server version	8.0.42-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ogloszenie`
+--
+
+DROP TABLE IF EXISTS `ogloszenie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ogloszenie` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `data_dodania` timestamp NULL DEFAULT NULL,
+  `tresc` text,
+  `ilosc_wyswietlen` int DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ogloszenie`
+--
+
+LOCK TABLES `ogloszenie` WRITE;
+/*!40000 ALTER TABLE `ogloszenie` DISABLE KEYS */;
+INSERT INTO `ogloszenie` VALUES (1,'2025-07-30 11:25:20','dodane w sql nie przez post',40),(2,'2025-07-30 13:14:04','przykładowe ogłoszenie dodane postem próba 3',5),(4,'2025-07-30 13:21:42','kolejne przykładowe ogłoszenie dodane postem ',1),(5,'2025-07-30 13:26:38','nowa zawartość ogłoszenia',4),(6,'2025-07-30 17:24:08','\"ogłoszenie dodane za pomocą menu endpoints w idea\"',0),(7,'2025-07-30 17:30:47',NULL,0),(8,'2025-07-30 17:38:58',NULL,0),(9,'2025-07-30 17:39:19',NULL,0),(10,'2025-07-30 17:40:04',NULL,0),(11,'2025-07-30 17:49:36',NULL,1),(12,'2025-07-30 19:02:54','nowa treść testowa',4),(15,'2025-07-30 19:05:44','test wyświetleń',1),(17,'2025-07-30 19:05:54','test wyświetleń',1),(19,'2025-07-30 19:06:04','test wyświetleń',1),(21,'2025-07-30 19:09:40','test wyświetleń',1),(23,'2025-07-30 19:15:14','test wyświetleń',1);
+/*!40000 ALTER TABLE `ogloszenie` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-30 23:08:34
